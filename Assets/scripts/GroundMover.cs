9bfa85c0422c9aff365ba80703f3e8d96859dfd6
gioiann <giovyiannacci@gmail.com>
2020-09-14 23:34:57 +0200

    Fixed Ground stutter

---
+++ Assets/scripts/GroundMover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GroundMover : MonoBehaviour
+{
+    public float speed = 1.66614f;
+
+    void Update()
+    {
+        foreach (Transform child in transform)
+        {
+            child.position += Vector3.left * speed * Time.deltaTime;
+            if (child.position.x <= -14.96f)
+            {
+                child.position += new Vector3(29.92f, 0, 0);
+            }
+        }
+    }
+}

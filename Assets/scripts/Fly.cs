b48a466c32a313fc7f3445664cd7ca4cfb2a0dff
gioiann <giovyiannacci@gmail.com>
2020-09-16 23:40:57 +0200

    Bird rotation and sounds
    Added bird Point and Swing sounds.

--- Assets/scripts/Fly.cs
+++ Assets/scripts/Fly.cs
@@ -2,19 +2,33 @@
 
 public class Fly : MonoBehaviour
 {
-    public float velocity = 3.5f;
+    public float velocity = 3.7f;
     public float sinDivider = 100f;
     public float sinSpeed = 8.1954591f;
+    public float downRotSpeed = 100f;
+    public float upRotSpeed = 350f;
 
-    private float sinTime = 0;
     private Rigidbody2D rb;
+    private Quaternion downRot;
+    private Quaternion upRot;
     private float lastJump = 0f;
     private bool release = true;
+    private float sinTime = 0;
+
+    private AudioSource audioSource;
+    private AudioClip point;
+    private AudioClip wing;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.isKinematic = true;
+        downRot = Quaternion.Euler(0, 0, -90f);
+        upRot = Quaternion.Euler(0, 0, 20f);
+
+        audioSource = GetComponent<AudioSource>();
+        point = Resources.Load<AudioClip>("sounds/sfx_point");
+        wing = Resources.Load<AudioClip>("sounds/sfx_wing");
     }
 
     void Update()
@@ -25,8 +39,9 @@ public class Fly : MonoBehaviour
             {
                 rb.isKinematic = false;
                 release = false;
+                audioSource.PlayOneShot(wing);
+                lastJump = Time.time + 0.100f;
                 rb.velocity = Vector2.up * velocity;
-                lastJump = Time.time + 0.200f;
             }
         }
         else
@@ -34,10 +49,32 @@ public class Fly : MonoBehaviour
             release = true;
         }
 
+
         if (rb.isKinematic == true)
         {
             transform.position += Vector3.up * Mathf.Sin(sinTime) / sinDivider;
             sinTime += Time.deltaTime * sinSpeed;
+        }
+        else
+        {
+            Rotate();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        audioSource.PlayOneShot(point);
+    }
+
+    private void Rotate()
+    {
+        if (rb.velocity.y <= -1)
+        {
+            rb.transform.rotation = Quaternion.RotateTowards(rb.transform.rotation, downRot, -rb.velocity.y * downRotSpeed * Time.deltaTime);
+        }
+        else if (rb.velocity.y > 0)
+        {
+            rb.transform.rotation = Quaternion.RotateTowards(rb.transform.rotation, upRot, rb.velocity.y * upRotSpeed * Time.deltaTime);
         }
     }
 }
